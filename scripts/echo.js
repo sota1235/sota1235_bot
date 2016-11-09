cdb2ac51fc99d8001ac0dff05d8b01844c59a399
sota1235 <sota1235@gmail.com>
2016-11-10 00:10:32 +0900

    modify: implement more sample

--- scripts/echo.js
+++ scripts/echo.js
@@ -3,7 +3,13 @@
  */
 
 module.exports = slackbot => {
+  slackbot.hear(/echo (.+)/, msg => {
+    console.log(msg);
+    msg.send(msg.match[1]);
+  });
+
   slackbot.respond(/echo (.+)/, msg => {
+    console.log(msg);
     slackbot.send(msg.match[1]);
   });
 };
