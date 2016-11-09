09cd82685cd2b2d6bcf5e02b666a83ef56e4ef67
sota1235 <sota1235@gmail.com>
2016-11-09 23:35:04 +0900

    add: template config

---
+++ config.sample.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview Config for bot.
+ */
+
+module.exports = {
+  SLACK_TOKEN           : '{Slack API token of your team}',
+  SLACK_DEFAULT_CHANNEL : '{Default channel of your team}',
+};
