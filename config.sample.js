050adf8910703cca6fc21599bb0284e43fa31e21
sota1235 <sota1235@gmail.com>
2016-11-10 00:10:23 +0900

    modify: add config key

--- config.sample.js
+++ config.sample.js
@@ -5,4 +5,5 @@
 module.exports = {
   SLACK_TOKEN           : '{Slack API token of your team}',
   SLACK_DEFAULT_CHANNEL : '{Default channel of your team}',
+  PORT                  : '{Listen port of hubot}',
 };
