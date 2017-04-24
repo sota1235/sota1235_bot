4242f3ac568d3d19fd5d3257adbcac1a3d507e13
sota1235 <sota1235@gmail.com>
2017-04-25 00:19:10 +0900

    modify: add config

---
+++ config.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview Config for bot.
+ */
+
+module.exports = {
+  SLACK_TOKEN           : 'xoxb-101770949201-smnLvQzdDnl5wxOoMalZqDSI',
+  SLACK_DEFAULT_CHANNEL : '#general',
+  PORT                  : '3333',
+};
--- config.sample.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview Config for bot.
- */
-
-module.exports = {
-  SLACK_TOKEN           : '{Slack API token of your team}',
-  SLACK_DEFAULT_CHANNEL : '{Default channel of your team}',
-  PORT                  : '{Listen port of hubot}',
-};
