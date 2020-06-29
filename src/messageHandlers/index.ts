c5f344d293526f5a352d58d9119d34289918c337
sota1235 <sota1235@gmail.com>
2020-06-30 00:39:58 +0900

    fix: register remo handler

--- src/messageHandlers/index.ts
+++ src/messageHandlers/index.ts
@@ -1,6 +1,8 @@
 import { App } from '@slack/bolt';
 import { registerEchoHandler } from './echo';
+import { registerRemoHandler } from './remo';
 
 export const registerMessageHandlers = (app: App) => {
   registerEchoHandler(app);
+  registerRemoHandler(app);
 };
