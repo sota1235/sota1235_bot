94feb91caa23896ccf03245a4347905fd89ff28f
sota1235 <sota1235@gmail.com>
2020-07-07 18:10:44 +0900

    modify: fix way to specify redis information

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -1,8 +1,3 @@
 import Redis from 'ioredis';
 
-export const Client = new Redis({
-  port: Number(process.env.REDIS_PORT),
-  host: process.env.REDIS_HOST,
-  username: process.env.REDIS_USER,
-  password: process.env.REDIS_PASSWORD,
-});
+export const Client = new Redis(process.env.REDIS_URL);
