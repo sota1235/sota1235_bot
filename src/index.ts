e2f14e74d5884dbe8906d2189dcbda94ca1a321b
sota1235 <sota1235@gmail.com>
2021-10-03 13:42:18 +0900

    modify: use sentry and use heroku lab

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,16 @@ import reactionAddedHandlers from './reaction_handlers';
 import { registerMessageHandlers } from './messageHandlers';
 import { registerSchedulers } from './scheduler';
 import { channels } from './constants';
-import { getDeployInfo } from './config';
+import Sentry, { Severity } from '@sentry/node';
 
+// Sentry initialization
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+  debug: process.env.NODE_ENV !== 'production',
+});
+
+// App initialization
 const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET as string,
 });
@@ -14,6 +22,18 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
 });
 
+app.error(async (err) => {
+  console.error(err);
+  Sentry.captureException(err, {
+    level: Severity.Critical,
+  });
+});
+
+receiver.router.get('/liveness_check', (_, res) => {
+  res.send('OK');
+});
+
+// Register event handlers
 registerMessageHandlers(app);
 
 app.event<'reaction_added'>('reaction_added', async (args) => {
@@ -27,10 +47,7 @@ app.event<'reaction_added'>('reaction_added', async (args) => {
   }
 });
 
-receiver.router.get('/liveness_check', (_, res) => {
-  res.send('OK');
-});
-
+// Start
 (async () => {
   // Start the app
   await app.start(3000);
@@ -41,8 +58,10 @@ receiver.router.get('/liveness_check', (_, res) => {
 
   await app.client.chat.postMessage({
     channel: channels.sandbox,
-    text: `Botがデプロイされました ${
-      getDeployInfo()?.SOURCE_VERSION || 'revision_not_found'
+    text: `Botがデプロイされました
+    hash: ${process.env.HEROKU_SLUG_COMMIT || 'revision not found'}
+    release version: ${
+      process.env.HEROKU_RELEASE_VERSION || 'version not found'
     }`,
     token: process.env.SLACK_BOT_TOKEN,
     icon_emoji: ':wrench:',
